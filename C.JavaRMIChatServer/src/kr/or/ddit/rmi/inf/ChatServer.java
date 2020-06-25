cab606734ec1570c6bbfbbd5953e7ffd7686e872
sua <sua@DESKTOP-4KKIEIJ>
2020-06-25 20:20:07 +0900

    .

---
+++ C.JavaRMIChatServer/src/kr/or/ddit/rmi/inf/ChatServer.java
@@ -0,0 +1,15 @@
+package kr.or.ddit.rmi.inf;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ChatServer extends Remote {
+
+	// 사용자 추가
+	public void addClient(ChatClient client, String name) throws RemoteException;
+
+	public void disconnect(ChatClient client, String name) throws RemoteException;
+
+	public void say(String messsage) throws RemoteException;
+
+}
